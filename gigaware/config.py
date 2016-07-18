b836e3cc29956b39625c4e8b68aea71bd93e32ad
Shai Wilson <sjwilson2@usfca.edu>
2016-07-17 20:29:17 -0600

    add sessions.js, change account.html

--- gigaware/config.py
+++ gigaware/config.py
@@ -2,13 +2,20 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class DefaultConfig(object):
-    SECRET_KEY = 'secret-key'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'not-so-secret')
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
     TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
     TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
     TWILIO_NUMBER = os.environ['TWILIO_NUMBER']
     APPLICATION_SID = os.environ['APPLICATION_SID']
+    # authy
+    # AUTHY_API_KEY = os.environ.get('AUTHY_API_KEY')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class DevelopmentConfig(DefaultConfig):
