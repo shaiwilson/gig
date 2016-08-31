86e6aeea403cf44ed25c205bb4cf0259e54bbe12
Bhargav Nookala <nooknb@gmail.com>
2016-08-30 18:20:08 -0700

    Adding redis caching for translations

--- gigaware/translator/__init__.py
+++ gigaware/translator/__init__.py
@@ -1,15 +1,30 @@
 from translator import Translator
 from bing_translator import BingTranslator
+import redis
+
+gig_redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+TARGET_TRANSLATION_LANG = 'de'
 
 gig_translator = Translator()
 gig_translator.set_translator(BingTranslator())
-gig_translator.set_target_translation_language('de')
+gig_translator.set_target_translation_language(TARGET_TRANSLATION_LANG)
+
+
+def build_localizable_key(untranslated, target_translation_lang):
+    return 'giglocalizable%%%' + untranslated + '%%%' + target_translation_lang
+
 
 def translate(untranslated):
-    return gig_translator.translate(untranslated)
+    key = build_localizable_key(untranslated, TARGET_TRANSLATION_LANG)
 
-def translate_all(batched_untranslated):
-    return gig_translator.translate_all(batched_untranslated)
+    # If redis contains the localized string in the target language, return it.
+    localized = gig_redis.get(key)
+    if localized:
+        return localized.decode("utf-8")
 
-def test_translate(test):
-    return gig_translator.test_translate(test)
+    # Otherwise, ask bing for the translation, and set the returned translation
+    # in redis.
+    localized = gig_translator.translate(untranslated)
+    gig_redis.set(key, localized)
+    return localized.decode("utf-8")
